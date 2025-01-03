53918a2f32910bb34f9bfe5f6cca9be9d2a87fc3
Vinicius Maia <viniciushm97@gmail.com>
2025-01-03 20:34:45 +0000

    Setup of Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+
+prisma.user.create({
+    data:{
+        name: "Vinicius",
+        email: "viniciushm97@gmail.com"
+    }
+})
