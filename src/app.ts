8fe258c14e5a4b7470aa9d5d489f814153d28c87
Vinicius Maia <viniciushm97@gmail.com>
2025-01-05 13:57:37 +0000

    Creation of docker compose file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 prisma.user.create({
-    data:{
-        name: "Vinicius",
-        email: "viniciushm97@gmail.com"
-    }
-})
+  data: {
+    name: "Vinicius",
+    email: "viniciushm97@gmail.com",
+  },
+});
